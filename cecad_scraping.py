270ea92a1e1781e41ca49be2161544c355beac96
ricardormotta <raspinimottaricardo@gmail.com>
2021-01-25 10:32:27 -0300

    novas alteracoes

--- cecad_scraping.py
+++ cecad_scraping.py
@@ -10,7 +10,7 @@ uf_ibge=35
 lista_municipios=pd.read_excel("./RELATORIO_DTB_BRASIL_MUNICIPIO.xls")
 lista_municipios_UF=lista_municipios['Código Município Completo'][lista_municipios['UF']==uf_ibge].tolist()
 
-#%% 2) Definindo opções pro request
+#%% 2) Definição das opções pro request
 # OPÇÃO : name = value
 # MARCAÇÃO PBF: schema = PBF (Com marcação PBF)
 # ESTADO/REGIAO: uf_ibge = 12 (AC - Acre)
@@ -18,34 +18,12 @@ lista_municipios_UF=lista_municipios['Código Município Completo'][lista_munici
 # VARIAVEL COLUNA: var1 = 'fx_rft' (Faixa da renda familiar per capita)
 # VARIAVEL LINHA: var2 = '' (vazio para baixar apenas as faixas de renda familiar)
 
+#%% 3) Laço de repetição para baixar todos os dados de renda
 schema='PBF'
 uf_ibge=35
-p_ibge=3500105
-var1='fx_rfpc' 
-var2=''
-
-form_data = urllib.parse.urlencode({
-    'schema': schema,
-    'uf_ibge': uf_ibge,
-    'p_ibge': p_ibge,
-    'var1': var1,
-    'var2': var2}) 
-form_data = form_data.encode("utf-8")
-
-#%% 3) Carregando tabelas
-page_obj = urllib.request.urlopen(
-    "https://cecad.cidadania.gov.br/tab_cad_table.php?p_tipo=absoluto",
-    form_data)
-raw_data = page_obj.read().decode("utf-8")   # .decode("utf-8") converte para string
-#%% 4) Convertendo tabelas para dataframe
-tables_list = pd.read_html(raw_data,index_col=0,thousands='.')  # indexando pela cidade
-tab_familia = tables_list[0].dropna()  # separa a primeira tabela
-tab_pessoa = tables_list[1].dropna()  # separa a segunda tabela
-
-#%% 5) Laço de repetição para baixar todos os dados de renda
-
 renda_pessoa = pd.DataFrame()
 renda_familia = pd.DataFrame()
+
 var1='fx_rfpc' 
 var2=''
 
@@ -77,8 +55,11 @@ while i < (len(lista_municipios_UF)):
         time.sleep(10)
         continue
 
-#%% 5) Laço de repetição para baixar todos os população de rua
+#%% 4) Laço de repetição para baixar todos os população de rua
+schema='PBF'
+uf_ibge=35
 var1='marc_sit_rua'
+
 pop_rua_pessoa = pd.DataFrame()
 pop_rua_familia = pd.DataFrame()
 i=0
