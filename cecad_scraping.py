ea28d3aa14e1f8f7facdd95122cac55d51a190a3
ricardormotta <raspinimottaricardo@gmail.com>
2021-01-25 07:53:06 -0300

    Colocando os dados dos forms como variáveis

--- cecad_scraping.py
+++ cecad_scraping.py
@@ -1,8 +1,9 @@
+#%% 0) Importando as bibliotecas
 import urllib.request
 import urllib
 import pandas as pd
 
-# 1) Definindo opções pro request
+#%% 1) Definindo opções pro request
 # OPÇÃO : name = value
 # MARCAÇÃO PBF: schema = PBF (Com marcação PBF)
 # ESTADO/REGIAO: uf_ibge = 12 (AC - Acre)
@@ -10,16 +11,21 @@ import pandas as pd
 # VARIAVEL COLUNA: var1 = cod_est_cadastral_fam (Estado cadastral da família)
 # VARIAVEL LINHA: var2 = fx_rft (Faixa da renda total da família)
 
-#%%
+schema='PBF'
+uf_ibge=12
+p_ibge=1200013
+var1='cod_est_cadastral_fam'
+var2=''
+
 form_data = urllib.parse.urlencode({
-    'schema': 'PBF',
-    'uf_ibge': 12,
-    'p_ibge': 1200013,
-    'var1': 'cod_est_cadastral_fam',
-    'var2': ''}) 
+    'schema': schema,
+    'uf_ibge': uf_ibge,
+    'p_ibge': p_ibge,
+    'var1': var1,
+    'var2': var2}) 
 form_data = form_data.encode("utf-8")
 
-# 2) Carregando tabelas
+#%% 2) Carregando tabelas
 page_obj = urllib.request.urlopen(
     "https://cecad.cidadania.gov.br/tab_cad_table.php?p_tipo=absoluto",
     form_data)
@@ -29,3 +35,7 @@ raw_data = page_obj.read().decode("utf-8")   # .decode("utf-8") converte para st
 tables_list = pd.read_html(raw_data,index_col=0,thousands='.')  # indexando pela cidade
 tab_familia = tables_list[0].dropna()  # separa a primeira tabela
 tab_pessoa = tables_list[1].dropna()  # separa a segunda tabela
+
+#%% 4) Lendo a lista de municípios
+
+
