f215d124d497731c2a7ae4334d99a201240423b7
ricardormotta <raspinimottaricardo@gmail.com>
2021-01-25 10:25:52 -0300

    várias alteracoes, perdao pelo vacilo rs

--- cecad_scraping.py
+++ cecad_scraping.py
@@ -2,6 +2,7 @@
 import urllib.request
 import urllib
 import pandas as pd
+import time
 
 
 #%% 1) Lendo a lista de municípios e transformando em lista (indexável)
@@ -41,31 +42,79 @@ tables_list = pd.read_html(raw_data,index_col=0,thousands='.')  # indexando pela
 tab_familia = tables_list[0].dropna()  # separa a primeira tabela
 tab_pessoa = tables_list[1].dropna()  # separa a segunda tabela
 
+#%% 5) Laço de repetição para baixar todos os dados de renda
 
+renda_pessoa = pd.DataFrame()
+renda_familia = pd.DataFrame()
+var1='fx_rfpc' 
+var2=''
 
-#%% 5) Laço de repetição para baixar todos os dados
-valores_pessoa = pd.DataFrame()
-valores_familia = pd.DataFrame()
-for i in range(len(lista_municipios_UF)):
-    form_data = urllib.parse.urlencode({
-    'schema': schema,
-    'uf_ibge': uf_ibge,
-    'p_ibge': lista_municipios_UF[i],
-    'var1': var1,
-    'var2': var2}) 
+i=0
+while i < (len(lista_municipios_UF)):
+    try:
+        form_data = urllib.parse.urlencode({
+        'schema': schema,
+        'uf_ibge': uf_ibge,
+        'p_ibge': lista_municipios_UF[i],
+        'var1': var1,
+        'var2': var2}) 
 
-    form_data = form_data.encode("utf-8")
-    page_obj = urllib.request.urlopen(
-    "https://cecad.cidadania.gov.br/tab_cad_table.php?p_tipo=absoluto",
-    form_data)
-    raw_data = page_obj.read().decode("utf-8")
-    tables_list = pd.read_html(raw_data,index_col=0,thousands='.')  # indexando pela cidade
-    tab_familia = tables_list[0].dropna()  # separa a primeira tabela
-    tab_pessoa = tables_list[1].dropna()  # separa a segunda tabela
-    valores_familia=valores_familia.append(tab_familia.iloc[[0]]) # Cria o df para familia
-    valores_pessoa=valores_pessoa.append(tab_pessoa.iloc[[0]]) # Cria o df para pessoa
-    print(i)
+        form_data = form_data.encode("utf-8")
+        page_obj = urllib.request.urlopen(
+        "https://cecad.cidadania.gov.br/tab_cad_table.php?p_tipo=absoluto",
+        form_data)
+        raw_data = page_obj.read().decode("utf-8")
+        tables_list = pd.read_html(raw_data,index_col=0,thousands='.')  # indexando pela cidade
+        tab_familia = tables_list[0].dropna()  # separa a primeira tabela
+        tab_pessoa = tables_list[1].dropna()  # separa a segunda tabela
+        renda_familia=renda_familia.append(tab_familia.iloc[[0]]) # Cria o df para familia
+        renda_pessoa=renda_pessoa.append(tab_pessoa.iloc[[0]]) # Cria o df para pessoa
+        print(i)
+        i+=1
+    except ValueError:
+        print('Esperando 10s pra tentar novamente...')
+        print('Erro na tentativa ' , i)
+        time.sleep(10)
+        continue
 
+#%% 5) Laço de repetição para baixar todos os população de rua
+var1='marc_sit_rua'
+pop_rua_pessoa = pd.DataFrame()
+pop_rua_familia = pd.DataFrame()
+i=0
 
+while i < (len(lista_municipios_UF)):
+    try:
+        form_data = urllib.parse.urlencode({
+        'schema': schema,
+        'uf_ibge': uf_ibge,
+        'p_ibge': lista_municipios_UF[i],
+        'var1': var1,
+        'var2': var2}) 
 
-# %%
+        form_data = form_data.encode("utf-8")
+        page_obj = urllib.request.urlopen(
+        "https://cecad.cidadania.gov.br/tab_cad_table.php?p_tipo=absoluto",
+        form_data)
+        raw_data = page_obj.read().decode("utf-8")
+        tables_list = pd.read_html(raw_data,index_col=0,thousands='.')  # indexando pela cidade
+        tab_familia = tables_list[0].dropna()  # separa a primeira tabela
+        tab_pessoa = tables_list[1].dropna()  # separa a segunda tabela
+        pop_rua_familia=pop_rua_familia.append(tab_familia.iloc[[0]]) # Cria o df para familia
+        pop_rua_pessoa=pop_rua_pessoa.append(tab_pessoa.iloc[[0]]) # Cria o df para pessoa
+        print(i)
+        i+=1
+    except ValueError:
+        print('Esperando 10s pra tentar novamente...')
+        print('Erro na tentativa ' , i)
+        time.sleep(10)
+        continue
+
+#%% 6) Salvando em excel
+writer=pd.ExcelWriter('Faixa de renda pessoa e familia completo.xlsx', engine='xlsxwriter')
+renda_familia.to_excel(writer, sheet_name='FaixaRendaFamilia')
+renda_pessoa.to_excel(writer, sheet_name='FaixaRendaPessoa')
+pop_rua_familia.to_excel(writer, sheet_name='PopRuaFamilia')
+pop_rua_pessoa.to_excel(writer, sheet_name='PopRuaPessoa')
+writer.save()
+
