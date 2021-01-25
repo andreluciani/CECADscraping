82876a78d6bda43378f4711df5db5020220910ce
ricardormotta <raspinimottaricardo@gmail.com>
2021-01-25 08:18:35 -0300

    adicionando a lista de municípios em list

--- cecad_scraping.py
+++ cecad_scraping.py
@@ -13,8 +13,8 @@ import pandas as pd
 # VARIAVEL LINHA: var2 = fx_rft (Faixa da renda total da família)
 
 schema='PBF'
-uf_ibge=12
-p_ibge=1200013
+uf_ibge=35
+p_ibge=3500105
 var1='cod_est_cadastral_fam'
 var2=''
 
@@ -31,13 +31,13 @@ page_obj = urllib.request.urlopen(
     "https://cecad.cidadania.gov.br/tab_cad_table.php?p_tipo=absoluto",
     form_data)
 raw_data = page_obj.read().decode("utf-8")   # .decode("utf-8") converte para string
-#%%
-# 3) Convertendo tabelas para dataframe
+#%% 3) Convertendo tabelas para dataframe
 tables_list = pd.read_html(raw_data,index_col=0,thousands='.')  # indexando pela cidade
 tab_familia = tables_list[0].dropna()  # separa a primeira tabela
 tab_pessoa = tables_list[1].dropna()  # separa a segunda tabela
 
-#%% 4) Lendo a lista de municípios
-pd.read_excel("./RELATORIO_DTB_BRASIL_MUNICIPIO.ods",engine="odf")
+#%% 4) Lendo a lista de municípios e transformando em lista (indexável)
+lista_municipios=pd.read_excel("./RELATORIO_DTB_BRASIL_MUNICIPIO.xls",sheet_name='DTB_2018_Municipio')
+lista_municipios_UF=lista_municipios['Código Município Completo'][lista_municipios['UF']==uf_ibge].tolist()
 
-
+# %%
