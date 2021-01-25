d179a69843bd9c258601be59204798a6fd3d48b0
ricardormotta <raspinimottaricardo@gmail.com>
2021-01-25 08:26:20 -0300

    ordem da lista de municipios no início

--- cecad_scraping.py
+++ cecad_scraping.py
@@ -2,9 +2,14 @@
 import urllib.request
 import urllib
 import pandas as pd
-# Necessário instalar 'odfpy' 
 
-#%% 1) Definindo opções pro request
+
+#%% 1) Lendo a lista de municípios e transformando em lista (indexável)
+uf_ibge=35
+lista_municipios=pd.read_excel("./RELATORIO_DTB_BRASIL_MUNICIPIO.xls",sheet_name='DTB_2018_Municipio')
+lista_municipios_UF=lista_municipios['Código Município Completo'][lista_municipios['UF']==uf_ibge].tolist()
+
+#%% 2) Definindo opções pro request
 # OPÇÃO : name = value
 # MARCAÇÃO PBF: schema = PBF (Com marcação PBF)
 # ESTADO/REGIAO: uf_ibge = 12 (AC - Acre)
@@ -26,18 +31,16 @@ form_data = urllib.parse.urlencode({
     'var2': var2}) 
 form_data = form_data.encode("utf-8")
 
-#%% 2) Carregando tabelas
+#%% 3) Carregando tabelas
 page_obj = urllib.request.urlopen(
     "https://cecad.cidadania.gov.br/tab_cad_table.php?p_tipo=absoluto",
     form_data)
 raw_data = page_obj.read().decode("utf-8")   # .decode("utf-8") converte para string
-#%% 3) Convertendo tabelas para dataframe
+#%% 4) Convertendo tabelas para dataframe
 tables_list = pd.read_html(raw_data,index_col=0,thousands='.')  # indexando pela cidade
 tab_familia = tables_list[0].dropna()  # separa a primeira tabela
 tab_pessoa = tables_list[1].dropna()  # separa a segunda tabela
 
-#%% 4) Lendo a lista de municípios e transformando em lista (indexável)
-lista_municipios=pd.read_excel("./RELATORIO_DTB_BRASIL_MUNICIPIO.xls",sheet_name='DTB_2018_Municipio')
-lista_municipios_UF=lista_municipios['Código Município Completo'][lista_municipios['UF']==uf_ibge].tolist()
+
 
 # %%
